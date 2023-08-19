bb98d24d77f21cbd5a80347be3d6523418470014
Anmol-Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-08-19 18:08:48 +0530

    feat: convert country code to full country name

--- src/features/fetch-geolocation.ts
+++ src/features/fetch-geolocation.ts
@@ -1,4 +1,5 @@
-// GeolocationService.ts
+import countryList from 'country-list';   // we can also use: i18n-iso-countries package
+
 const fetchGeolocation = async () => {
     try {
       // to get ip address
@@ -26,7 +27,9 @@ const fetchGeolocation = async () => {
       const locData = await locResponse.json() // sample output: {"ip": "49.42.95.190", "city": "Sonīpat", "region": "Haryana", "country": "IN", "loc": "28.9948,77.0194", "org": "AS55836 Reliance Jio Infocomm Limited", "postal": "131001", "timezone": "Asia/Kolkata"}
       const { city, country } = locData
   
-      return { city, country };
+      const countryFullName = countryList.getName(country);
+
+      return { city, country: countryFullName };
     } catch (error) {
       throw new Error("Error fetching geolocation: " + error.message);
     }
