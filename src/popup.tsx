3ef84eda4536c48a3882f706804fe6e1c827c3c2
Anmol-Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-08-19 17:50:13 +0530

    feat: seperate fetching location logic

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react"
 
+import fetchGeolocation from "~features/fetch-geolocation"
 import { LocationButton } from "~features/location-button"
 import { ShowLocation } from "~features/show-location"
 
@@ -17,30 +18,7 @@ function IndexPopup() {
     setLoading(true)
     setErrorMsg("")
     try {
-      // to get ip address
-      const Response = await fetch("https://api.ipify.org?format=json") // sample output: {"ip":"98.207.254.136"}
-
-      // good for displaying custom message
-      if (!Response.ok) {
-        throw new Error("Error fetching IP data")
-      }
-
-      const ipData = await Response.json()
-
-      // as denoted by the sample output
-      const ipVal = ipData.ip
-
-      // call ipinfo.io API to get the geolocation
-      const locResponse = await fetch(
-        `https://ipinfo.io/${ipVal}?token=${process.env.PLASMO_PUBLIC_IPINFO_API_TOKEN}`
-      )
-
-      if (!locResponse.ok) {
-        throw new Error("Error fetching location data")
-      }
-
-      const locData = await locResponse.json() // sample output: {"ip": "49.42.95.190", "city": "Sonīpat", "region": "Haryana", "country": "IN", "loc": "28.9948,77.0194", "org": "AS55836 Reliance Jio Infocomm Limited", "postal": "131001", "timezone": "Asia/Kolkata"}
-      const { city, country } = locData
+      const { city, country } = await fetchGeolocation()
 
       setCity(city)
       setCountry(country)
