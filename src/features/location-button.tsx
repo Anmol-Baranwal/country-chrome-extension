95ebd51835bf0963952f069a6fcdfda02ef091b1
Anmol-Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-08-19 14:31:36 +0530

    feat: use icon and add initial structure of props

--- src/features/location-button.tsx
+++ src/features/location-button.tsx
@@ -1,13 +1,33 @@
+import { faSpinner } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import React, { type FC } from "react"
 
 interface ShowMyLocationButtonProps {
   txt: string
+  loading: boolean
+  handleFetching: () => void
 }
 
-export const LocationButton: FC<ShowMyLocationButtonProps> = ({ txt }) => {
+export const LocationButton: FC<ShowMyLocationButtonProps> = ({
+  txt,
+  loading,
+  handleFetching
+}) => {
   return (
     <div>
-      <button>{txt}</button>
+      <button
+        onClick={handleFetching}
+        className="plasmo-flex plasmo-items-center plasmo-px-4 plasmo-py-2 plasmo-text-sm plasmo-rounded-lg plasmo-transition-all plasmo-border-none
+      plasmo-shadow-lg hover:plasmo-shadow-md plasmo-mr-3">
+        {loading ? (
+          <>
+            <FontAwesomeIcon icon={faSpinner} spin size="lg" />
+            Loading ...
+          </>
+        ) : (
+          <>{txt}</>
+        )}
+      </button>
     </div>
   )
 }
--- src/popup.tsx
+++ src/popup.tsx
@@ -7,9 +7,15 @@ import "~style.css"
 function IndexPopup() {
   const [loading, setLoading] = useState<boolean>(false)
 
+  const handleFetchLocation = () => {}
+
   return (
     <div className="plasmo-flex plasmo-items-center plasmo-justify-center plasmo-h-[500px] plasmo-w-[500px]">
-      <LocationButton txt="Show My Location" />
+      <LocationButton
+        txt="Show My Location"
+        loading={loading}
+        handleFetching={handleFetchLocation}
+      />
     </div>
   )
 }
